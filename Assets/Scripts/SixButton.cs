f1804f80021c74618273fc9aedf7eb793995ce97
RCarScar <90071713+RCarScar@users.noreply.github.com>
2022-03-08 16:12:19 -0800

    Update SixButton.cs

--- Assets/Scripts/SixButton.cs
+++ Assets/Scripts/SixButton.cs
@@ -17,7 +17,7 @@ public class SixButton : MonoBehaviour
     public GameObject stage1;
     public GameObject stage2;
     public GameObject stage3;
-    public string first = "Game has Started, click the first box to continue";
+    public string first = "The party stops at a state park and searches for supplies. How many times do you want your party to run over the area to search for supplies?";
     public string second = "loser";
     public string third = "bad";
     public string fourth = "child";
@@ -26,6 +26,7 @@ public class SixButton : MonoBehaviour
     public string seventh = "nobody likes you";
     public string eighth = "you're sad";
     public string ninth = "bad.";
+    public bool[] done = { false, false, false, false, false, false, false, false, false, };
     // Start is called before the first frame update
     void Start()
     {
@@ -47,51 +48,61 @@ public class SixButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(TMPComponent.text == "")
+        if (TMPComponent.text == "" && done[0] == false)
         {
             TMPComponent.text = first;
+            done[0] = true;
         }
-        else if(TMPComponent.text == first)
+        else if (TMPComponent.text == first && done[1] == false)
         {
             stage += 1;
+            done[1] = true;
         }
-        else if (TMPComponent.text == second)
+        else if (TMPComponent.text == second && done[2] == false)
         {
             stage += 1;
+            done[2] = true;
         }
-        else if (TMPComponent.text == third)
+        else if (TMPComponent.text == third && done[3] == false)
         {
             stage += 1;
+            done[3] = true;
         }
-        else if (TMPComponent.text == fourth)
+        else if (TMPComponent.text == fourth && done[4] == false)
         {
             stage += 1;
+            done[4] = true;
         }
-        else if (TMPComponent.text == fifth)
+        else if (TMPComponent.text == fifth && done[5] == false)
         {
             stage += 1;
+            done[5] = true;
         }
-        else if (TMPComponent.text == sixth)
+        else if (TMPComponent.text == sixth && done[6] == false)
         {
             stage += 1;
+            done[6] = true;
         }
-        else if (TMPComponent.text == seventh)
+        else if (TMPComponent.text == seventh && done[7] == false)
         {
             stage += 1;
+            done[7] = true;
         }
-        else if (TMPComponent.text == eighth)
+        else if (TMPComponent.text == eighth && done[8] == false)
         {
             stage += 1;
+            done[8] = true;
         }
-        else if (TMPComponent.text == ninth)
+        else if (TMPComponent.text == ninth && done[9] == false)
         {
             stage += 1;
+            done[9] = true;
         }
     }
     public void button1()
     {
         if (stage == 1) this.GetComponent<TextMeshPro>().text = ("1");
-        if (stage == 2) this.GetComponent<TextMeshPro>().text = ("1");
+        if (stage == 2) this.GetComponent<TextMeshPro>().text = ("beg");
         if (stage == 3) this.GetComponent<TextMeshPro>().text = ("1");
         if (stage == 4) this.GetComponent<TextMeshPro>().text = ("1");
         if (stage == 5) this.GetComponent<TextMeshPro>().text = ("1");
@@ -102,8 +113,8 @@ public class SixButton : MonoBehaviour
     }
     public void button2()
     {
-        if (stage == 1) this.GetComponent<TextMeshPro>().text = ("1");
-        if (stage == 2) this.GetComponent<TextMeshPro>().text = ("1");
+        if (stage == 1) this.GetComponent<TextMeshPro>().text = ("2");
+        if (stage == 2) this.GetComponent<TextMeshPro>().text = ("steal");
         if (stage == 3) this.GetComponent<TextMeshPro>().text = ("1");
         if (stage == 4) this.GetComponent<TextMeshPro>().text = ("1");
         if (stage == 5) this.GetComponent<TextMeshPro>().text = ("1");
@@ -114,9 +125,9 @@ public class SixButton : MonoBehaviour
     }
     public void button3()
     {
-        if (stage == 1) this.GetComponent<TextMeshPro>().text = ("1");
+        if (stage == 1) this.GetComponent<TextMeshPro>().text = ("3");
         if (stage == 2) this.GetComponent<TextMeshPro>().text = ("1");
-        if (stage == 3) this.GetComponent<TextMeshPro>().text = ("1");
+        if (stage == 3) this.GetComponent<TextMeshPro>().text = ("kill");
         if (stage == 4) this.GetComponent<TextMeshPro>().text = ("1");
         if (stage == 5) this.GetComponent<TextMeshPro>().text = ("1");
         if (stage == 6) this.GetComponent<TextMeshPro>().text = ("1");
@@ -126,7 +137,7 @@ public class SixButton : MonoBehaviour
     }
     public void button4()
     {
-        if (stage == 1) this.GetComponent<TextMeshPro>().text = ("1");
+        if (stage == 1) this.GetComponent<TextMeshPro>().text = ("4");
         if (stage == 2) this.GetComponent<TextMeshPro>().text = ("1");
         if (stage == 3) this.GetComponent<TextMeshPro>().text = ("1");
         if (stage == 4) this.GetComponent<TextMeshPro>().text = ("1");
@@ -138,7 +149,7 @@ public class SixButton : MonoBehaviour
     }
     public void button5()
     {
-        if (stage == 1) this.GetComponent<TextMeshPro>().text = ("1");
+        if (stage == 1) this.GetComponent<TextMeshPro>().text = (" . . . ");
         if (stage == 2) this.GetComponent<TextMeshPro>().text = ("1");
         if (stage == 3) this.GetComponent<TextMeshPro>().text = ("1");
         if (stage == 4) this.GetComponent<TextMeshPro>().text = ("1");
@@ -150,7 +161,7 @@ public class SixButton : MonoBehaviour
     }
     public void button6()
     {
-        if (stage == 1) this.GetComponent<TextMeshPro>().text = ("1");
+        if (stage == 1) this.GetComponent<TextMeshPro>().text = (" . . . ");
         if (stage == 2) this.GetComponent<TextMeshPro>().text = ("1");
         if (stage == 3) this.GetComponent<TextMeshPro>().text = ("1");
         if (stage == 4) this.GetComponent<TextMeshPro>().text = ("1");
